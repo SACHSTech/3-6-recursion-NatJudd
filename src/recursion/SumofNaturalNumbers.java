e17a8c534e382c2d5ec043b70729e67583478407
NatJudd <nathaniel.judd25@ycdsbk12.ca>
2025-05-13 14:24:18 -0400

    solved sum of natural numbers and fixed formatting in reverse a string

--- src/recursion/SumofNaturalNumbers.java
+++ src/recursion/SumofNaturalNumbers.java
@@ -6,7 +6,7 @@ public class SumofNaturalNumbers {
             return 0;
         }
 
-        return n + sum(n-1);
+        return n + sum(n - 1);
     }
 
     public static void main(String[] args) {
