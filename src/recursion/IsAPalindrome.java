9541be9935cbdfe3413114f8f76642be4af97729
NatJudd <nathaniel.judd25@ycdsbk12.ca>
2025-05-14 11:15:41 -0400

    Solved Check if a String is a Palindrome

---
+++ src/recursion/IsAPalindrome.java
@@ -0,0 +1,21 @@
+package recursion;
+
+public class IsAPalindrome {
+    public static boolean isPalindrome(String s) {
+        if (s.length() < 2) {
+            return true;
+        }
+
+        if (s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        }
+
+        return isPalindrome(s.substring(1, s.length() - 1));
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("leafs"));
+        System.out.println(isPalindrome("stats"));
+    }
+}
